082c4e0ad8903a163f372a4a8ce516edd3e69197
lidor-s <dor.siso92@gmail.com>
2018-06-24 16:40:30 +0300

    arrays

--- t1.asm
+++ t1.asm
@@ -1,1 +1,490 @@
-line 3: syntax error
+.data
+errorOutOfBounds: .asciiz "Error index out of bounds
+"
+errorZeroDiv: .asciiz "Error division by zero
+"
+
+.text
+
+.globl main
+.ent  main
+main:
+
+move $fp, $sp
+jal _main_
+label_7:
+li $v0,10
+syscall
+.end main
+
+
+.globl _main_
+.ent  _main_
+_main_:
+
+move $fp, $sp
+subu $fp, $fp, 4
+li $t0, 0
+subu $sp, $sp, 8
+sw $t0, ($sp)
+sw $t0, 4($sp)
+label_23:
+li $t0, 0
+li $t1, 1
+bge $t0, 2, labelOutOfRange
+blt $t0, 0, labelOutOfRange
+li $t2, 0
+addu $t2, $t2, $t0
+mul $t2, $t2, 4
+subu $t2, $fp, $t2
+sw $t1, ($t2)
+label_33:
+li $t2, 1
+li $t3, 2
+bge $t2, 2, labelOutOfRange
+blt $t2, 0, labelOutOfRange
+li $t4, 0
+addu $t4, $t4, $t2
+mul $t4, $t4, 4
+subu $t4, $fp, $t4
+sw $t3, ($t4)
+label_43:
+li $t4, 0
+subu $sp, $sp, 8
+sw $t4, ($sp)
+sw $t4, 4($sp)
+label_48:
+li $t4, 0
+bge $t4, 2, labelOutOfRange
+blt $t4, 0, labelOutOfRange
+li $t5, 2
+addu $t5, $t5, $t4
+mul $t5, $t5, 4
+subu $t5, $fp, $t5
+lw $t5, ($t5)
+subu $sp, $sp, 4
+sw $t0, ($sp)
+subu $sp, $sp, 4
+sw $t1, ($sp)
+subu $sp, $sp, 4
+sw $t2, ($sp)
+subu $sp, $sp, 4
+sw $t3, ($sp)
+subu $sp, $sp, 4
+sw $t4, ($sp)
+subu $sp, $sp, 4
+sw $t5, ($sp)
+subu $sp, $sp, 4
+sw $t6, ($sp)
+subu $sp, $sp, 4
+sw $t7, ($sp)
+subu $sp, $sp, 4
+sw $s0, ($sp)
+subu $sp, $sp, 4
+sw $s1, ($sp)
+subu $sp, $sp, 4
+sw $s2, ($sp)
+subu $sp, $sp, 4
+sw $s3, ($sp)
+subu $sp, $sp, 4
+sw $s4, ($sp)
+subu $sp, $sp, 4
+sw $s5, ($sp)
+subu $sp, $sp, 4
+sw $s6, ($sp)
+subu $sp, $sp, 4
+sw $s7, ($sp)
+subu $sp, $sp, 4
+sw $t8, ($sp)
+subu $sp, $sp, 4
+sw $t9, ($sp)
+subu $sp, $sp, 4
+sw $fp, ($sp)
+subu $sp, $sp, 4
+sw $ra, ($sp)
+subu $sp, $sp, 4
+sw $t5, ($sp)
+jal _printi_
+lw $t5, ($sp)
+addu $sp, $sp, 4
+lw $ra, ($sp)
+addu $sp, $sp, 4
+lw $fp, ($sp)
+addu $sp, $sp, 4
+lw $t9, ($sp)
+addu $sp, $sp, 4
+lw $t8, ($sp)
+addu $sp, $sp, 4
+lw $s7, ($sp)
+addu $sp, $sp, 4
+lw $s6, ($sp)
+addu $sp, $sp, 4
+lw $s5, ($sp)
+addu $sp, $sp, 4
+lw $s4, ($sp)
+addu $sp, $sp, 4
+lw $s3, ($sp)
+addu $sp, $sp, 4
+lw $s2, ($sp)
+addu $sp, $sp, 4
+lw $s1, ($sp)
+addu $sp, $sp, 4
+lw $s0, ($sp)
+addu $sp, $sp, 4
+lw $t7, ($sp)
+addu $sp, $sp, 4
+lw $t6, ($sp)
+addu $sp, $sp, 4
+lw $t5, ($sp)
+addu $sp, $sp, 4
+lw $t4, ($sp)
+addu $sp, $sp, 4
+lw $t3, ($sp)
+addu $sp, $sp, 4
+lw $t2, ($sp)
+addu $sp, $sp, 4
+lw $t1, ($sp)
+addu $sp, $sp, 4
+lw $t0, ($sp)
+addu $sp, $sp, 4
+label_142:
+li $t6, 1
+bge $t6, 2, labelOutOfRange
+blt $t6, 0, labelOutOfRange
+li $t7, 2
+addu $t7, $t7, $t6
+mul $t7, $t7, 4
+subu $t7, $fp, $t7
+lw $t7, ($t7)
+subu $sp, $sp, 4
+sw $t0, ($sp)
+subu $sp, $sp, 4
+sw $t1, ($sp)
+subu $sp, $sp, 4
+sw $t2, ($sp)
+subu $sp, $sp, 4
+sw $t3, ($sp)
+subu $sp, $sp, 4
+sw $t4, ($sp)
+subu $sp, $sp, 4
+sw $t5, ($sp)
+subu $sp, $sp, 4
+sw $t6, ($sp)
+subu $sp, $sp, 4
+sw $t7, ($sp)
+subu $sp, $sp, 4
+sw $s0, ($sp)
+subu $sp, $sp, 4
+sw $s1, ($sp)
+subu $sp, $sp, 4
+sw $s2, ($sp)
+subu $sp, $sp, 4
+sw $s3, ($sp)
+subu $sp, $sp, 4
+sw $s4, ($sp)
+subu $sp, $sp, 4
+sw $s5, ($sp)
+subu $sp, $sp, 4
+sw $s6, ($sp)
+subu $sp, $sp, 4
+sw $s7, ($sp)
+subu $sp, $sp, 4
+sw $t8, ($sp)
+subu $sp, $sp, 4
+sw $t9, ($sp)
+subu $sp, $sp, 4
+sw $fp, ($sp)
+subu $sp, $sp, 4
+sw $ra, ($sp)
+subu $sp, $sp, 4
+sw $t7, ($sp)
+jal _printi_
+lw $t7, ($sp)
+addu $sp, $sp, 4
+lw $ra, ($sp)
+addu $sp, $sp, 4
+lw $fp, ($sp)
+addu $sp, $sp, 4
+lw $t9, ($sp)
+addu $sp, $sp, 4
+lw $t8, ($sp)
+addu $sp, $sp, 4
+lw $s7, ($sp)
+addu $sp, $sp, 4
+lw $s6, ($sp)
+addu $sp, $sp, 4
+lw $s5, ($sp)
+addu $sp, $sp, 4
+lw $s4, ($sp)
+addu $sp, $sp, 4
+lw $s3, ($sp)
+addu $sp, $sp, 4
+lw $s2, ($sp)
+addu $sp, $sp, 4
+lw $s1, ($sp)
+addu $sp, $sp, 4
+lw $s0, ($sp)
+addu $sp, $sp, 4
+lw $t7, ($sp)
+addu $sp, $sp, 4
+lw $t6, ($sp)
+addu $sp, $sp, 4
+lw $t5, ($sp)
+addu $sp, $sp, 4
+lw $t4, ($sp)
+addu $sp, $sp, 4
+lw $t3, ($sp)
+addu $sp, $sp, 4
+lw $t2, ($sp)
+addu $sp, $sp, 4
+lw $t1, ($sp)
+addu $sp, $sp, 4
+lw $t0, ($sp)
+addu $sp, $sp, 4
+label_236:
+li $t0, 0
+mul $t0, $t0, 4
+subu $t0, $fp, $t0
+li $t1, 2
+mul $t1, $t1, 4
+subu $t1, $fp, $t1
+lw $t2, ($t0)
+sw $t2, ($t1)
+subu $t0, $t0, 4
+subu $t1, $t1, 4
+lw $t2, ($t0)
+sw $t2, ($t1)
+subu $t0, $t0, 4
+subu $t1, $t1, 4
+label_251:
+li $s0, 0
+bge $s0, 2, labelOutOfRange
+blt $s0, 0, labelOutOfRange
+li $s1, 2
+addu $s1, $s1, $s0
+mul $s1, $s1, 4
+subu $s1, $fp, $s1
+lw $s1, ($s1)
+subu $sp, $sp, 4
+sw $t0, ($sp)
+subu $sp, $sp, 4
+sw $t1, ($sp)
+subu $sp, $sp, 4
+sw $t2, ($sp)
+subu $sp, $sp, 4
+sw $t3, ($sp)
+subu $sp, $sp, 4
+sw $t4, ($sp)
+subu $sp, $sp, 4
+sw $t5, ($sp)
+subu $sp, $sp, 4
+sw $t6, ($sp)
+subu $sp, $sp, 4
+sw $t7, ($sp)
+subu $sp, $sp, 4
+sw $s0, ($sp)
+subu $sp, $sp, 4
+sw $s1, ($sp)
+subu $sp, $sp, 4
+sw $s2, ($sp)
+subu $sp, $sp, 4
+sw $s3, ($sp)
+subu $sp, $sp, 4
+sw $s4, ($sp)
+subu $sp, $sp, 4
+sw $s5, ($sp)
+subu $sp, $sp, 4
+sw $s6, ($sp)
+subu $sp, $sp, 4
+sw $s7, ($sp)
+subu $sp, $sp, 4
+sw $t8, ($sp)
+subu $sp, $sp, 4
+sw $t9, ($sp)
+subu $sp, $sp, 4
+sw $fp, ($sp)
+subu $sp, $sp, 4
+sw $ra, ($sp)
+subu $sp, $sp, 4
+sw $s1, ($sp)
+jal _printi_
+lw $s1, ($sp)
+addu $sp, $sp, 4
+lw $ra, ($sp)
+addu $sp, $sp, 4
+lw $fp, ($sp)
+addu $sp, $sp, 4
+lw $t9, ($sp)
+addu $sp, $sp, 4
+lw $t8, ($sp)
+addu $sp, $sp, 4
+lw $s7, ($sp)
+addu $sp, $sp, 4
+lw $s6, ($sp)
+addu $sp, $sp, 4
+lw $s5, ($sp)
+addu $sp, $sp, 4
+lw $s4, ($sp)
+addu $sp, $sp, 4
+lw $s3, ($sp)
+addu $sp, $sp, 4
+lw $s2, ($sp)
+addu $sp, $sp, 4
+lw $s1, ($sp)
+addu $sp, $sp, 4
+lw $s0, ($sp)
+addu $sp, $sp, 4
+lw $t7, ($sp)
+addu $sp, $sp, 4
+lw $t6, ($sp)
+addu $sp, $sp, 4
+lw $t5, ($sp)
+addu $sp, $sp, 4
+lw $t4, ($sp)
+addu $sp, $sp, 4
+lw $t3, ($sp)
+addu $sp, $sp, 4
+lw $t2, ($sp)
+addu $sp, $sp, 4
+lw $t1, ($sp)
+addu $sp, $sp, 4
+lw $t0, ($sp)
+addu $sp, $sp, 4
+label_345:
+li $s2, 1
+bge $s2, 2, labelOutOfRange
+blt $s2, 0, labelOutOfRange
+li $s3, 2
+addu $s3, $s3, $s2
+mul $s3, $s3, 4
+subu $s3, $fp, $s3
+lw $s3, ($s3)
+subu $sp, $sp, 4
+sw $t0, ($sp)
+subu $sp, $sp, 4
+sw $t1, ($sp)
+subu $sp, $sp, 4
+sw $t2, ($sp)
+subu $sp, $sp, 4
+sw $t3, ($sp)
+subu $sp, $sp, 4
+sw $t4, ($sp)
+subu $sp, $sp, 4
+sw $t5, ($sp)
+subu $sp, $sp, 4
+sw $t6, ($sp)
+subu $sp, $sp, 4
+sw $t7, ($sp)
+subu $sp, $sp, 4
+sw $s0, ($sp)
+subu $sp, $sp, 4
+sw $s1, ($sp)
+subu $sp, $sp, 4
+sw $s2, ($sp)
+subu $sp, $sp, 4
+sw $s3, ($sp)
+subu $sp, $sp, 4
+sw $s4, ($sp)
+subu $sp, $sp, 4
+sw $s5, ($sp)
+subu $sp, $sp, 4
+sw $s6, ($sp)
+subu $sp, $sp, 4
+sw $s7, ($sp)
+subu $sp, $sp, 4
+sw $t8, ($sp)
+subu $sp, $sp, 4
+sw $t9, ($sp)
+subu $sp, $sp, 4
+sw $fp, ($sp)
+subu $sp, $sp, 4
+sw $ra, ($sp)
+subu $sp, $sp, 4
+sw $s3, ($sp)
+jal _printi_
+lw $s3, ($sp)
+addu $sp, $sp, 4
+lw $ra, ($sp)
+addu $sp, $sp, 4
+lw $fp, ($sp)
+addu $sp, $sp, 4
+lw $t9, ($sp)
+addu $sp, $sp, 4
+lw $t8, ($sp)
+addu $sp, $sp, 4
+lw $s7, ($sp)
+addu $sp, $sp, 4
+lw $s6, ($sp)
+addu $sp, $sp, 4
+lw $s5, ($sp)
+addu $sp, $sp, 4
+lw $s4, ($sp)
+addu $sp, $sp, 4
+lw $s3, ($sp)
+addu $sp, $sp, 4
+lw $s2, ($sp)
+addu $sp, $sp, 4
+lw $s1, ($sp)
+addu $sp, $sp, 4
+lw $s0, ($sp)
+addu $sp, $sp, 4
+lw $t7, ($sp)
+addu $sp, $sp, 4
+lw $t6, ($sp)
+addu $sp, $sp, 4
+lw $t5, ($sp)
+addu $sp, $sp, 4
+lw $t4, ($sp)
+addu $sp, $sp, 4
+lw $t3, ($sp)
+addu $sp, $sp, 4
+lw $t2, ($sp)
+addu $sp, $sp, 4
+lw $t1, ($sp)
+addu $sp, $sp, 4
+lw $t0, ($sp)
+addu $sp, $sp, 4
+jr $ra
+.end _main_
+
+
+.globl _print_
+.ent  _print_
+_print_:
+
+move $fp, $sp
+subu $fp, $fp, 4
+lw $a0,0($sp)
+li $v0,4
+syscall
+jr $ra
+.end _print_
+
+
+.globl _printi_
+.ent  _printi_
+_printi_:
+
+move $fp, $sp
+subu $fp, $fp, 4
+lw $a0,0($sp)
+li $v0,1
+syscall
+jr $ra
+.end _printi_
+
+labelZeroDiv:
+la $s4, errorZeroDiv
+subu $sp, $sp, 4
+sw $s4, ($sp)
+jal _print_
+li $v0,10
+syscall
+
+labelOutOfRange:
+la $s4, errorOutOfBounds
+subu $sp, $sp, 4
+sw $s4, ($sp)
+jal _print_
+li $v0,10
+syscall
